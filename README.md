c35b104d74b1d6ec40b7d119d7ae0f1f64541b4c
liliangrong777 <1198072497@qq.com>
2022-09-27 23:57:46 +0800

    feat: update README

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# punctuate
-A robust repository for replacing punctuation. it has no dependencies.
+# Punctuate
+
+Punctuate is a robust library that can be used to replace punctuation. It has no dependencies, only 2kb, easy to use and supports custom symbols.
+
+## Usage
+
+You will need `Node.js` installed on your system.
+
+```shell
+npm i punctuate -S  
+```
+
+```javascript
+const { replacePunctuateByMap } = require('punctuate')
+
+const text = 'mom said "Knowledge makes humble， ignorance makes proud。"'
+const replaceMap = {
+  '，': ',',
+  '。': '.'
+}
+const newText = replacePunctuateByMap(text, replaceMap)
+console.log(newText); // mom said "Knowledge makes humble, ignorance makes proud."
+
+```
+
+Or manually download and link **punctuate.js** in your HTML, It can also be downloaded via [UNPKG](https://unpkg.com/punctuate/dist/):
+
+```html
+<script src="https://unpkg.com/punctuate/dist/punctuate.js"></script>
+<script>
+  const { replacePunctuateByMap } = PunctuateReplace
+  const text = 'mom said "Knowledge makes humble， ignorance makes proud。"'
+  const replaceMap = {
+    '，': ',',
+    '。': '.'
+  }
+  const newText = replacePunctuateByMap(text, replaceMap)
+  console.log(newText); // mom said "Knowledge makes humble, ignorance makes proud."
+</script>
+```
+
+**Predefine**
+
+If you feel that it is too cumbersome to fill in the mapping, punctuate has built-in mapping table for Chinese and English exchange. You just need to use replacePunctuate to pass the replacement keyword in the second parameter.for example:
+
+```javascript
+const { replacePunctuate, DEFAULT_CHARACTER } = require('punctuate');
+const text = 'mom said "Knowledge makes humble， ignorance makes proud。"'
+const newText = replacePunctuate(text, 'ZH2EN')
+console.log(newText); // mom said "Knowledge makes humble, ignorance makes proud."
+console.log(DEFAULT_CHARACTER) // log predefined map
+
+```
+
+If you want to know which replacement punctuation is predefined, try printing `DEFAULT_CHARACTER`.
--- test/punctuate.test.ts
+++ test/punctuate.test.ts
@@ -1,7 +1,4 @@
-import { PunctuateReplacer, replacePunctuateByMap } from '../src'
-import { DEFAULT_CHARACTER } from '../src/constants'
-import type { Replacer } from '../src/utils';
-import { replaceByPredefined } from '../src/utils'
+import { PunctuateReplacer, replacePunctuate, replacePunctuateByMap, Replacer, DEFAULT_CHARACTER } from '../src'
 
 const testedZhSentence = '人生不如意事十之八九,凡事不必太过认真,不要太过强求."得知坦然,失之淡然,争之必然,顺其自然."一切随缘,顺其自然就好.命里有时终须有,命里无时莫强求.的确,这是一个尔虞我诈,充满竞争的社会,适者生存,不适者淘汰.'
 const expectZhSentence = '人生不如意事十之八九，凡事不必太过认真，不要太过强求。“得知坦然，失之淡然，争之必然，顺其自然。”一切随缘，顺其自然就好。命里有时终须有，命里无时莫强求。的确，这是一个尔虞我诈，充满竞争的社会，适者生存，不适者淘汰。'
@@ -21,8 +18,8 @@ describe('测试PunctuateReplace', () => {
   describe('使用构造函数，生成一个替换器实例', () => {
     it('传递map生成一个替换器', () => {
       const punctuate = new PunctuateReplacer(DEFAULT_CHARACTER.EN2ZH)
-      const newText = punctuate.replace(testedZhSentence)
-      expect(newText).toBe(expectZhSentence)
+      const expectedText = punctuate.replace(testedZhSentence)
+      expect(expectedText).toBe(expectZhSentence)
     })
     it('如果传入的replacer类型错误，则抛出异常', () => {
       const errorReplacer = '.-。'
@@ -33,10 +30,10 @@ describe('测试PunctuateReplace', () => {
   })
   describe('测试使用replacePunctuateByMap传入map对象', () => {
     it('传入预设的DEFAULT_CHARACTER.EN2ZH，DEFAULT_CHARACTER.ZH2EN', () => {
-      const newText = replacePunctuateByMap(zhText, DEFAULT_CHARACTER.EN2ZH)
-      expect(newText).toBe(expectZhSentence)
-      const newText2 = replacePunctuateByMap(enText, DEFAULT_CHARACTER.ZH2EN)
-      expect(newText2).toBe(expectEnSentence)
+      const expectedText = replacePunctuateByMap(zhText, DEFAULT_CHARACTER.EN2ZH)
+      expect(expectedText).toBe(expectZhSentence)
+      const expectedText2 = replacePunctuateByMap(enText, DEFAULT_CHARACTER.ZH2EN)
+      expect(expectedText2).toBe(expectEnSentence)
     })
     it('传入自定义map,替换英文逗号和英文句号', () => {
       const customMap = {
@@ -44,27 +41,37 @@ describe('测试PunctuateReplace', () => {
         ',': '，'
       }
       zhText = '生于忧患,死于安乐.'
-      const newText = replacePunctuateByMap(zhText, customMap)
-      expect(newText).toBe('生于忧患，死于安乐。')
+      const expectedText = replacePunctuateByMap(zhText, customMap)
+      expect(expectedText).toBe('生于忧患，死于安乐。')
     })
     it('对于引号这种成对的，map的值使用数组', () => {
       const customMap = {
         '"': ['“', '”']
       }
       zhText = '然则何时而乐耶？其必曰"先天下之忧而忧，后天下之乐而乐"乎！'
-      const newText = replacePunctuateByMap(zhText, customMap)
-      expect(newText).toBe('然则何时而乐耶？其必曰“先天下之忧而忧，后天下之乐而乐”乎！')
+      const expectedText = replacePunctuateByMap(zhText, customMap)
+      expect(expectedText).toBe('然则何时而乐耶？其必曰“先天下之忧而忧，后天下之乐而乐”乎！')
     })
   })
-  describe('使用replaceByPredefined传入预定义关键字', () => {
+  describe('使用replacePunctuate传入预定义关键字', () => {
     it('预定义关键字包含 EN2ZH、ZH2EN', () => {
-      const newText = replaceByPredefined(zhText, 'EN2ZH')
-      expect(newText).toBe(expectZhSentence)
-      const newText2 = replaceByPredefined(enText, 'ZH2EN')
-      expect(newText2).toBe(expectEnSentence)
+      const expectedText = replacePunctuate(zhText, 'EN2ZH')
+      expect(expectedText).toBe(expectZhSentence)
+      const expectedText2 = replacePunctuate(enText, 'ZH2EN')
+      expect(expectedText2).toBe(expectEnSentence)
+    })
+    it('测试预定义英文字符都能正常转换为中文', () => {
+      const text = '. ? ! , \ ; : ~ [ ] { } < > ( ) | \' " "'
+      const expectedText = '。 ？ ！ ，  ； ： ～ 【 】 「 」 《 》 （ ） ｜ ‘ “ ”'
+      expect(replacePunctuate(text, 'EN2ZH')).toBe(expectedText)
+    })
+    it('测试与定义中文字符都能正常转换为英文', () => {
+      const text = '。 ？ ！ ， 、 ； ： ～ 【 】 《 》 （ ） ｜ 「 」 “ ” ‘ ’'
+      const expectedText = '. ? ! , \\ ; : ~ [ ] < > ( ) | { } " " \' \''
+      expect(replacePunctuate(text, 'ZH2EN')).toBe(expectedText)
     })
     it('预定义的关键字目前只有[EN2ZH,ZH2EN]，其他关键字将报错', () => {
-      expect(() => replaceByPredefined(zhText, 'WRONG_KEY' as unknown as 'EN2ZH')).toThrow()
+      expect(() => replacePunctuate(zhText, 'WRONG_KEY' as unknown as 'EN2ZH')).toThrow()
     })
   })
 })
